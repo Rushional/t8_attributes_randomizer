604765dee6a340735b1e46f28b5ca9a62e7d4dc3
Rushional <Rushional2@gmail.com>
2022-07-09 12:37:21 +0200

    first commit

---
+++ src/main/java/com/rushional/t8_attributes_randomizer/T8AttributesRandomizerApplication.java
@@ -0,0 +1,13 @@
+package com.rushional.t8_attributes_randomizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T8AttributesRandomizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(T8AttributesRandomizerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rushional/t8_attributes_randomizer/T8AttributesRandomizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rushional.t8_attributes_randomizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T8AttributesRandomizerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
